32f998e76b6c1280e1605021bc827ac280838e2a
Mazen Kassem <kassemmazen@gmail.com>
2026-09-24 10:10:51 -0400

    Restrict local Vite servers to loopback

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: {
+    host: '127.0.0.1',
+  },
+
+  preview: {
+    host: '127.0.0.1',
+  },
+
   build: {
     license: {
       fileName: 'licenses.md',
